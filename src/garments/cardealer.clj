cfbbc8e6262c3d9921aa6caf6d7422593e348c95
kpenov16 <s133967@student.dtu.dk>
2019-11-28 20:12:06 +0100

    alternative solution of the car dealer chalenge :)

--- src/garments/cardealer.clj
+++ src/garments/cardealer.clj
@@ -11,13 +11,51 @@
   (if (or (<= discount 0) (> discount 1))
     (do
       (println "You can't get a discount!")
+      (println "budget: " budget)
       (doseq [[k price] cars]
         (if (canbuy budget price) (println "You can buy " k " for: " price))))
     (do
       (println "You have a valid discount of: " discount)
+      (println "budget: " budget)
       (doseq [[k price] cars]
         (if (canbuy budget price) (println "You can buy " k " for: " (- price (* price discount))))))
     ))
 (getoffer 61000 0.3)
 
-
+;;alternative solution, more or less the same as mine
+(defn isCodeValid
+  [code]
+  (defstruct coupon :Name :Discount)
+  (def validCoupon (struct coupon "20Percent" 0.8))
+  (if (= (:Name validCoupon) code)
+    true
+    false)
+  )
+;; validCoupon use is funny
+(defn getCarPrices
+  [budget code]
+  (def cars {"bmw" 60000 "ferrari" 100000 "fiat" 20000})
+  (if (isCodeValid code)
+    (do
+      (println "The code is valid")
+      (def discount (:Discount validCoupon))
+      (doseq [car cars]
+        (def carName (first car))
+        (def carPrice (second car))
+        (def priceDiscount (* carPrice discount))
+        (if (<= priceDiscount budget)
+          (println "The" carName "costs" priceDiscount))
+        )
+      )
+    (do
+      (println "The code is invalid")
+      (doseq [car cars]
+        (def carName (first car))
+        (def carPrice (last car))
+        (if (<= carPrice budget)
+          (println "The" carName "costs" carPrice))
+        )
+      )
+    )
+  )
+(getCarPrices 50000 "20Percent")
