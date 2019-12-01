9daa0fe5f1bc26b7a61387cc9fee29f2e25ae77f
kpenov16 <s133967@student.dtu.dk>
2019-12-01 07:10:05 +0100

    write failing test for givenAllScoredOne_return20

--- src/garments/bowling/core_test.clj
+++ src/garments/bowling/core_test.clj
@@ -5,6 +5,9 @@
 
 (deftest score-test
   (testing "given10Missed_returnZero"
-    (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ])))))
+    (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
+  (testing "givenAllScoredOne_return20"
+    (is (= 0 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]))))
+  )
 
 (run-tests 'garments.bowling.core-test)
