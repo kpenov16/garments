f94ab99b2901474548d4a3460d063d55fa9c1c0f
kpenov16 <s133967@student.dtu.dk>
2019-12-01 20:39:20 +0100

    refactor the failing test

--- src/garments/bowling/core_test.clj
+++ src/garments/bowling/core_test.clj
@@ -10,8 +10,8 @@
 
 (deftest to-frame-scores-test
   (testing "givenFrames_returnScores")
-  (is (= [[2] [2] [2] [2] [2] [2] [2] [2] [2] [2]]
-         (to-frame-scores [[1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]) ))
+  (is (= [1 2 3 4 5 6 7 8 9 0]
+         (to-frame-scores [[1 0] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]) ))
   )
 
 #_(deftest score-test
