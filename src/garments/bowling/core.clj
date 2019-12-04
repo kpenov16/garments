fb1005ab1fef43d0b184f010e6c83b514f329216
kpenov16 <s133967@student.dtu.dk>
2019-12-04 20:57:41 +0100

    make strike pass

--- src/garments/bowling/core.clj
+++ src/garments/bowling/core.clj
@@ -3,7 +3,17 @@
   )
 
 (defn group-frames [rolls]
-  (into [] (partition 2 rolls))
+  (loop [remaining rolls
+         frames []]
+    (if (empty? remaining)
+      frames
+      (if (= 10 (first remaining))
+          (recur (drop 1 remaining)
+                 (conj frames (take 1 remaining)))
+          (recur (drop 2 remaining)
+                 (conj frames (take 2 remaining)))
+          )
+      ))
   )
 
 (defn spare? [frame]
@@ -13,11 +23,16 @@
 (defn spare-extra-score [remaining-frames]
   (first (first remaining-frames)))
 
+(defn strike-extra-score [remaining-frames]
+  (reduce + (take 2 (flatten remaining-frames))))
+
 (defn next-frame-score [[frame & other]]
   (let [simple-score (reduce + frame)]
-    (if (spare? frame)
+     (if (= 10 (first frame))
+       (+ simple-score (strike-extra-score other))
+       (if (spare? frame)
       (+ simple-score (spare-extra-score other))
-      simple-score)))
+      simple-score)) ))
 
 
 (defn to-frame-scores [frames]
--- src/garments/bowling/core_test.clj
+++ src/garments/bowling/core_test.clj
@@ -3,16 +3,29 @@
             [garments.bowling.core :refer :all]))
 
 (deftest group-frames-test
-  (testing "givenRolls_returnRollsGroupedIntoFrames")
-  (is (= [[1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]
-         (group-frames [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]) ))
+  (testing "givenRolls_returnRollsGroupedIntoFrames"
+    (is (= [[1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]
+           (group-frames [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]) )))
+  (testing "givenRollsWithStrike_returnRollsGroupedIntoFrames"
+    (is (= [[10] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]
+           (group-frames [10 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]) )))
   )
 
 (deftest next-frame-score-test
-  (testing "givenNoSpareOrStrikeFrames_returnNextFrameScore")
-  (is (= 3 (next-frame-score [[1 2]])))
-  (testing "givenSpare_returnNextFrameScoreForASpare")
-  (is (= 14 (next-frame-score [[1 9] [4 2]]))))
+  (testing "givenNoSpareOrStrikeFrames_returnNextFrameScore"
+    (is (= 3 (next-frame-score [[1 2]]))))
+  (testing "givenSpare_returnNextFrameScoreForASpare"
+    (is (= 14 (next-frame-score [[1 9] [4 2]]))))
+  (testing "givenStrike_returnNextFrameScoreForAStrike"
+    (is (= 16 (next-frame-score [[10] [4 2]]))))
+  )
+
+
+(deftest strike-extra-score-test
+  (testing "givenNextFramesAfterAStrike_returnTheExtraStrikeScore")
+  (is (= 7 (strike-extra-score [[3 4]])) )
+  (is (= 2 (strike-extra-score [[1 1] [1 9]])))
+  )
 
 (deftest spare-extra-score-test
   (testing "givenNextFrames_returnTheExtraSpareScore")
@@ -28,12 +41,18 @@
   )
 
 (deftest to-frame-scores-test
-  (testing "givenFrames_returnScores")
-  (is (= [1 2 3 4 5 6 7 8 9 0]
-         (to-frame-scores [[1 0] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]) ))
-  (testing "givenSpareFrame_returnSpareScore")
-  (is (= [11 2 3 4 5 6 7 8 9 0]
-         (to-frame-scores [[1 9] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]) )))
+  (testing "givenFrames_returnScores"
+    (is (= [1 2 3 4 5 6 7 8 9 0]
+           (to-frame-scores [[1 0] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]) )))
+
+  (testing "givenSpareFrame_returnSpareScore"
+    (is (= [11 2 3 4 5 6 7 8 9 0]
+           (to-frame-scores [[1 9] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]) )))
+
+  (testing "givenStrikeFrame_returnStrikeScore"
+      (is (= [12 2 3 4 5 6 7 8 9 0]
+             (to-frame-scores [[10] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]) )))
+  )
 
 ;;#_
 (deftest score-test
@@ -44,6 +63,8 @@
     (is (= 50 (score [5 0 5 0 5 0 5 0 5 0 5 0 5 0 5 0 5 0 5 0 ]))))
   (testing "givenASpares_returnSumOfAll+FirstRollAfterSpareOnceAgain" ;;10+1+18
     (is (= 29 (score [9 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]))))
+  (testing "givenAStrike_returnSumOfAll+FirstTwoRollsAfterStrikeOnceAgain" ;;10+1+1+18
+    (is (= 30 (score [10 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]))))
   )
 
 (run-tests 'garments.bowling.core-test)
