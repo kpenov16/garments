9dad539e1bb3c07209b84cb9aa20d95ac730f16e
kpenov16 <s133967@student.dtu.dk>
2019-12-08 08:20:10 +0100

    make base test fail again

--- test/garments/kings/core_test.clj
+++ test/garments/kings/core_test.clj
@@ -7,13 +7,18 @@
 (facts "givenKingdomsAsJsonStr_returnMostPopularKingName"
        (fact "givenOneKingdomAsJsonStr_returnMostPopularKingIsThisKingsName"
              (king/most-popular-name "[\n {\n \"id\": 1,\n \"nm\": \"Edward the Elder\",\n \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"899-925\"\n  }]")
-              => "Edward")
-       (fact "givenNoKingdomsAsJsonStr_returnNil"
+              => "Edward"
+             (king/most-popular-name "[\n {\n \"id\": 1,\n \"nm\": \"Peter the Elder\",\n \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"899-925\"\n  }]")
+             => "Peter")
+       (fact "givenNoKingdomsAsJsonStr_returnEmptyList"
              (king/most-popular-name "[]")
              => ())
        (fact "givenNilAsJsonStr_returnNil"
              (king/most-popular-name nil)
              => nil)
+       (fact "givenInvalidJsonAsStr_returnException"
+             (king/most-popular-name "[")
+             => (throws Exception))
 #_       (fact "givenOneKingdomAsJsonStr_returnMostPopularKingIsThisKingsName"
       ( "[\n {\n \"id\": 1,\n \"nm\": \"Edward the Elder\",\n \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"899-925\"\n  },\n
              {\n \"id\": 2,\n \"nm\": \"Athelstan\",\n        \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"925-940\"\n  }]")
