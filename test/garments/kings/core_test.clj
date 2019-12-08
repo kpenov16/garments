d257b5a575af2edea1184093ad159850d78dc01f
kpenov16 <s133967@student.dtu.dk>
2019-12-08 07:48:19 +0100

    filing test for empty json

--- test/garments/kings/core_test.clj
+++ test/garments/kings/core_test.clj
@@ -7,8 +7,10 @@
 (facts "givenKingdomsAsJsonStr_returnMostPopularKingName"
        (fact "givenOneKingdomAsJsonStr_returnMostPopularKingIsThisKingsName"
              (king/most-popular-name "[\n {\n \"id\": 1,\n \"nm\": \"Edward the Elder\",\n \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"899-925\"\n  }]")
-              => "Edward"
-             )
+              => "Edward")
+       (fact "givenNoKingdomsAsJsonStr_returnNil"
+             (king/most-popular-name "[]")
+             => nil)
 #_       (fact "givenOneKingdomAsJsonStr_returnMostPopularKingIsThisKingsName"
       ( "[\n {\n \"id\": 1,\n \"nm\": \"Edward the Elder\",\n \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"899-925\"\n  },\n
              {\n \"id\": 2,\n \"nm\": \"Athelstan\",\n        \"cty\": \"United Kingdom\",\n  \"hse\": \"House of Wessex\",\n \"yrs\": \"925-940\"\n  }]")
