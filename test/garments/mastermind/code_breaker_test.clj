dee9aedd9ac118db03e82fe4ba1b730bd592d4b2
kpenov16 <s133967@student.dtu.dk>
2019-12-09 20:42:24 +0100

    refactor again and make them pass again

--- test/garments/mastermind/code_breaker_test.clj
+++ test/garments/mastermind/code_breaker_test.clj
@@ -3,11 +3,16 @@
            [garments.mastermind.code-breaker :refer :all]))
 
 (defn break-code [code guess]
-  [(count
-    (filter some?
-            (map
-              #(if (= %1 %2) :pos) code guess)))]
-  )
+  [(reduce + (map #(if (= (first %) (second %)) 1 0) (partition 2 (interleave code guess))))])
+
+;[0 0 0 0] [0 1 1 0]
+;(interleave [0 0 0 0] [0 1 1 0])
+;[0 0 0 1 0 1 0 0]
+;(partition 2 [0 0 0 1 0 1 0 0])
+;[0 0] [0 1] [0 1] [0 0]
+;(map #(if (= %1 %2) 1 0) [0 0] [0 1] [0 1] [0 0])
+;[1 0 0 1]
+;(reduce + [1 0 0 1])
 
 (fact "Code breaker"
       (fact "givenScoreGuessWithNoMatches_returnEmptyVector"
