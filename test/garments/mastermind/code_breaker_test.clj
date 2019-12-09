152b5f0db15cca4b2a4a31032dc1d421cb692e27
kpenov16 <s133967@student.dtu.dk>
2019-12-09 20:33:28 +0100

    refactor tests and make them pass again

--- test/garments/mastermind/code_breaker_test.clj
+++ test/garments/mastermind/code_breaker_test.clj
@@ -3,15 +3,20 @@
            [garments.mastermind.code-breaker :refer :all]))
 
 (defn break-code [code guess]
-  (filter some? (map #(if (= %1 %2) :pos) code guess))
+  [(count
+    (filter some?
+            (map
+              #(if (= %1 %2) :pos) code guess)))]
   )
 
 (fact "Code breaker"
       (fact "givenScoreGuessWithNoMatches_returnEmptyVector"
-             (break-code [0 0 0 0] [1 1 1 1]) => [])
+             (break-code [0 0 0 0] [1 1 1 1]) => [0])
       (fact "givenScoreOnePositionMatch_returnVectorWithMatchedPosition"
-             (break-code [0 0 0 0] [0 1 1 1]) => [:pos])
+             (break-code [0 0 0 0] [0 1 1 1]) => [1])
       (fact "givenScoreTwoPositionMatch_returnVectorWithTwoMatchedPositions"
-            (break-code [0 0 0 0] [0 0 1 1]) => [:pos :pos])
+            (break-code [0 0 0 0] [0 0 1 1]) => [2]
+            (break-code [0 0 0 0] [0 1 1 0]) => [2]
+            )
 )
 
