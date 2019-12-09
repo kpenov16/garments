2ea38c5522c253cbe4aea41125832c8258517469
kpenov16 <s133967@student.dtu.dk>
2019-12-09 20:01:47 +0100

    triangulate to make test fail

--- test/garments/mastermind/code_breaker_test.clj
+++ test/garments/mastermind/code_breaker_test.clj
@@ -3,9 +3,17 @@
            [garments.mastermind.code-breaker :refer :all]))
 
 (defn break-code [code guess]
-  []
+  (if (= (first code) (first guess))
+      [:pos]
+      [])
   )
 
 (fact "Code breaker"
-      (facts "givenScoreGuessWithNoMatches_returnEmptyVector")
-  (break-code [0 0 0 0] [1 1 1 1]) => [])
+      (fact "givenScoreGuessWithNoMatches_returnEmptyVector"
+             (break-code [0 0 0 0] [1 1 1 1]) => [])
+      (fact "givenScoreOnePositionMatch_returnVectorWithMatchedPosition"
+             (break-code [0 0 0 0] [0 1 1 1]) => [:pos])
+      (fact "givenScoreTwoPositionMatch_returnVectorWithTwoMatchedPositions"
+            (break-code [0 0 0 0] [0 0 1 1]) => [:pos :pos])
+)
+
