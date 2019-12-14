6669043fec41ebdad77f5130157fc9f7b27c528e
kpenov16 <s133967@student.dtu.dk>
2019-12-14 16:21:13 +0100

    adding some requirements

--- src/kpv_spaces/core.clj
+++ src/kpv_spaces/core.clj
@@ -6,6 +6,24 @@
 ; Where do you want new items to go?
 ; Does SequentialSpace should be an interface ?
 
+;requirements:
+;0) can have duplicates, sequential order
+;
+;1) putting tuples to spaces like: (put spaceX tupleX)
+;                               or (put (spaceX.) tupleX)
+;2) create empty sequential space
+;   something like: (def spaceX)
+;
+;3) search for tuple in sequential pSpace
+;   like: (queryp (spaceX.) (TupleInterface "tuple_key"))
+; returns a clone of the tuple else nil
+;
+; 4) removing tuples from sequential pSpace
+;    like: (getp (spaceX.) tupleX)
+;     and: (getp (spaceX.) "clean kitchen")
+; deletes the tuple from space and returns a clone
+; else nil
+
 
 (defprotocol Space
   (put [this t]))
